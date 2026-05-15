ab565e2e4d1e620b375bfa9c1044740369d929d4
Camilin28 <berbeocamilo@gmail.com>
2026-05-15 18:13:36 -0500

    actualizar bd

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$host = "mysql.railway.internal";
+$host = "mysql://root:BjJnNBPbpmyxmxCjYhXyrxYyzKnHSSkq@yamanote.proxy.rlwy.net:53260/railway";
 $dbname = "railway";
 $username = "root";
 $password = "BjJnNBPbpmyxmxCjYhXyrxYyzKnHSSkq";
