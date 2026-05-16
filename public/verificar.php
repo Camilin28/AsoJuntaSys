42413de18fbc954910e714652986ed1c9cdb1903
Camilin28 <berbeocamilo@gmail.com>
2026-05-16 14:14:39 -0500

    security: eliminar archivos de prueba y desarrollo sin proteccion

--- public/verificar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hash_guardado = '$2y$10$NDPEB3XVPcWNybnJn9loKu3v2I6CuZxAXcvS7wFy/zx...'; // Copia la contraseña cifrada desde la BD
-$contraseña_usuario = '246'; // Prueba con la contraseña real
-
-if (password_verify($contraseña_usuario, $hash_guardado)) {
-    echo "✅ Coincide";
-} else {
-    echo "❌ No coincide";
-}
